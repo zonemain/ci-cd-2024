30aae50e0475594c07df44a809e9dedaca8f0def
zonemain <zone.web@gmail.com>
2024-08-22 18:50:32 -0600

    CICD-1 Adding 2024 in response

--- src/main/java/com/jenkins_ci_cd/jenkins_ci_cd/Controller/CourseController.java
+++ src/main/java/com/jenkins_ci_cd/jenkins_ci_cd/Controller/CourseController.java
@@ -26,7 +26,7 @@ public class CourseController {
         Map<String, String> response = new HashMap<>();
         response.put("course", courseName);
         response.put("status", "success");
-        response.put("year", "2025");
+        response.put("year", "2024");
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
